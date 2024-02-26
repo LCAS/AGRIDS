db7a0edcbdb59f1d4fd0216194e24d8110c65679
Jonathan Cox <jcox289@gmail.com>
2024-02-26 12:08:50 +0000

    File name change

--- zenoh/minio/python_scripts/zenoh_subscribe_store_image.py
+++ zenoh/minio/python_scripts/zenoh_subscribe_store_image.py
@@ -3,8 +3,6 @@ import time
 from minio import Minio
 from io import BytesIO
 
-import orion_add_image
-
 # Connect to MinIO
 minio_client = Minio(
     "cabbage-xps-8900:9000",
@@ -14,28 +12,18 @@ minio_client = Minio(
 )
 
 # Define MinIO bucket name
-bucket_name = "zenoh" # should be vineyard name
+bucket_name = "zenoh"
 
-# Define the zenoh topic where the image data will be published
+# Define the topic where the image data will be published
 topic = "image_data/*"
 
 # Callback function to handle received image data
-def callback(data):
-    file_path = str(data.key_expr).split("/", 1)[-1] # Remove topic name
-    file_path = file_path.replace("_", "/")  # Replace underscores with slashes
-    vineyard_id, block_id, vinerow_id, vine_id, file_name = file_path.split("/")
+def handle_image_data(data):
 
+    file_name = str(data.key_expr).split("/", 1)[-1]
     image_data = data.payload
 
-    # TODO appednd time stamp to file name not overwrite exixsting files in MinIO and store filename_timestamp.ext or filename/timestamp.ext refrence in orion.
-    destination_file_name_minio = block_id + "/" + vinerow_id + "/" + vine_id + "/" + file_name
-    photo_id = file_name.split(".")[0]
-
-    # Create or update entity in Orion
-    orion_add_image.create_or_update_photo_entity(photo_id, vine_id, vinerow_id, block_id, vineyard_id, file_name)
-
-    # Store files in MinIO. Bucket == vineyard_id, structure blockID/vinerowID/vineID/file.ext
-    store_image_in_minio(image_data, destination_file_name_minio)
+    store_image_in_minio(image_data, file_name)
 
 # Function to store image in MinIO
 def store_image_in_minio(image_data, file_name):
@@ -46,5 +34,5 @@ def store_image_in_minio(image_data, file_name):
 
 if __name__ == "__main__":
     session = zenoh.open()
-    sub = session.declare_subscriber(topic, callback)
+    sub = session.declare_subscriber(topic, handle_image_data)
     time.sleep(60)
--- zenoh/minio/python_scripts/zenoh_subscribe_store_image_orion.py
+++ zenoh/minio/python_scripts/zenoh_subscribe_store_image_orion.py
@@ -3,6 +3,8 @@ import time
 from minio import Minio
 from io import BytesIO
 
+import orion_add_image
+
 # Connect to MinIO
 minio_client = Minio(
     "cabbage-xps-8900:9000",
@@ -12,18 +14,28 @@ minio_client = Minio(
 )
 
 # Define MinIO bucket name
-bucket_name = "zenoh"
+bucket_name = "zenoh" # should be vineyard name
 
-# Define the topic where the image data will be published
+# Define the zenoh topic where the image data will be published
 topic = "image_data/*"
 
 # Callback function to handle received image data
-def handle_image_data(data):
+def callback(data):
+    file_path = str(data.key_expr).split("/", 1)[-1] # Remove topic name
+    file_path = file_path.replace("_", "/")  # Replace underscores with slashes
+    vineyard_id, block_id, vinerow_id, vine_id, file_name = file_path.split("/")
 
-    file_name = str(data.key_expr).split("/", 1)[-1]
     image_data = data.payload
 
-    store_image_in_minio(image_data, file_name)
+    # TODO appednd time stamp to file name not overwrite exixsting files in MinIO and store filename_timestamp.ext or filename/timestamp.ext refrence in orion.
+    destination_file_name_minio = block_id + "/" + vinerow_id + "/" + vine_id + "/" + file_name
+    photo_id = file_name.split(".")[0]
+
+    # Create or update entity in Orion
+    orion_add_image.create_or_update_photo_entity(photo_id, vine_id, vinerow_id, block_id, vineyard_id, file_name)
+
+    # Store files in MinIO. Bucket == vineyard_id, structure blockID/vinerowID/vineID/file.ext
+    store_image_in_minio(image_data, destination_file_name_minio)
 
 # Function to store image in MinIO
 def store_image_in_minio(image_data, file_name):
@@ -34,5 +46,5 @@ def store_image_in_minio(image_data, file_name):
 
 if __name__ == "__main__":
     session = zenoh.open()
-    sub = session.declare_subscriber(topic, handle_image_data)
+    sub = session.declare_subscriber(topic, callback)
     time.sleep(60)
